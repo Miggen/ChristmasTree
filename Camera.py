578d9545531df570395d3afba0e2fe87ca8edab2
Mikael Hartman <hartman.mikael@gmail.com>
2024-12-22 15:43:06 +0100

    Fixate camera exposure

--- Camera.py
+++ Camera.py
@@ -1,13 +1,15 @@
 import cv2
 from time import sleep
+import matplotlib.pyplot as plt
 
 
 class Camera:
-    def __init__(self, visualize):
-        self._image_name = "preview"
-        self.visualize = visualize
-        cv2.namedWindow(self._image_name)
+    def __init__(self):
         self.stream = cv2.VideoCapture(0)
+        self.stream.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
+        self.stream.set(cv2.CAP_PROP_EXPOSURE, 128)
+        self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+        self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
         if self.stream.isOpened():
             status, frame = self.stream.read()
             self.height = frame.shape[0]
@@ -17,11 +19,18 @@ class Camera:
 
     def __del__(self):
         self.stream.release()
-        cv2.destroyAllWindows()
 
     def get(self):
         for _ in range(0, 6):
             status, frame = self.stream.read()
-        if self.visualize:
-            cv2.imshow(self._image_name, frame)
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         return frame
+
+
+if __name__ == "__main__":
+    camera = Camera()
+    image = camera.get()
+    plt.imshow(image)
+    plt.title("Camera Image")
+    plt.axis("off")
+    plt.show()
