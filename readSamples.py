42cce2856082d1772aa58438d41eca582b5fe760
Mikael Hartman <hartman.mikael@gmail.com>
2025-12-12 15:40:32 +0100

    Cleanup unused debug scripts

--- readSamples.py
+++
@@ -1,9 +0,0 @@
-import pickle
-from glob import glob
-
-for filename in glob('/home/pi/Data/sample_dmp_*.pkl'):
-    with open(filename, 'rb') as f:
-        samples = pickle.load(f)
-        for sample in samples:
-            print(sample)
-    print()
